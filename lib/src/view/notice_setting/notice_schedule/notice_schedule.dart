510a119bcb6c665931cab75b040ed1aad7e44cb6
kesojiso <kennaka7512@gmail.com>
2024-08-04 02:30:04 +0900

    refactor/separate notice_schedule.dart

--- lib/src/view/notice_setting/notice_schedule/notice_schedule.dart
+++ lib/src/view/notice_setting/notice_schedule/notice_schedule.dart
@@ -3,9 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../constants.dart';
 import '../../../model/setting_notice_schedule.dart';
-import 'schedle_dropdown_menu.dart';
-import '../../../model/setting_notice_schedule_input_form.dart';
 import '../../../component/warningbox.dart';
+import 'notice_timem_append_form.dart';
 
 class NoticeScheduleSettingPage extends ConsumerWidget {
   const NoticeScheduleSettingPage({super.key});
@@ -89,66 +88,3 @@ class NoticeScheduleSettingPage extends ConsumerWidget {
     );
   }
 }
-
-class NoticeTimeAppendForm extends ConsumerWidget {
-  const NoticeTimeAppendForm({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          shape: const CircleBorder(),
-          backgroundColor: buttonColor,
-        ),
-        child: const Icon(Icons.add, color: primaryColor, size: 90),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return Container(
-                height: 200,
-                color: buttonColor,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const Text('通知時刻を入力してください', style: normalTextStyle),
-                      const ScheduleInputForm(),
-                      ElevatedButton(
-                        child: const Text('追加'),
-                        onPressed: () {
-                          bool ans = ref
-                              .read(noticeScheduleProvider.notifier)
-                              .appendSchedule((
-                            ref.watch(noticeHourMinuteProvider).hour,
-                            ref.watch(noticeHourMinuteProvider).minute
-                          ));
-                          if (!ans) {
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(scheduleAlreadyExistSnackBar);
-                            return;
-                          }
-                          Navigator.pop(context);
-                          ref.invalidate(noticeHourMinuteProvider);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-const scheduleAlreadyExistSnackBar = SnackBar(
-  backgroundColor: Colors.red,
-  behavior: SnackBarBehavior.floating,
-  margin: EdgeInsets.only(bottom: 200, left: 20, right: 20),
-  content: Text("既に存在する時刻です"),
-);
---
+++ lib/src/view/notice_setting/notice_schedule/notice_timem_append_form.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../constants.dart';
+import 'schedle_dropdown_menu.dart';
+import '../../../model/setting_notice_schedule.dart';
+import '../../../model/setting_notice_schedule_input_form.dart';
+
+class NoticeTimeAppendForm extends ConsumerWidget {
+  const NoticeTimeAppendForm({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          backgroundColor: buttonColor,
+        ),
+        child: const Icon(Icons.add, color: primaryColor, size: 90),
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 200,
+                color: buttonColor,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('通知時刻を入力してください', style: normalTextStyle),
+                      const ScheduleInputForm(),
+                      ElevatedButton(
+                        child: const Text('追加'),
+                        onPressed: () {
+                          bool ans = ref
+                              .read(noticeScheduleProvider.notifier)
+                              .appendSchedule((
+                            ref.watch(noticeHourMinuteProvider).hour,
+                            ref.watch(noticeHourMinuteProvider).minute
+                          ));
+                          if (!ans) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(scheduleAlreadyExistSnackBar);
+                            return;
+                          }
+                          Navigator.pop(context);
+                          ref.invalidate(noticeHourMinuteProvider);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+const scheduleAlreadyExistSnackBar = SnackBar(
+  backgroundColor: Colors.red,
+  behavior: SnackBarBehavior.floating,
+  margin: EdgeInsets.only(bottom: 200, left: 20, right: 20),
+  content: Text("既に存在する時刻です"),
+);
