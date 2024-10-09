e1519dcdb259632eacd00b1071fd3a50202e91d4
kesojiso <kennaka7512@gmail.com>
2024-10-10 03:11:37 +0900

    ad_helperを微修正

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -18,7 +18,7 @@ class AdHelper {
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
-      throw new UnsupportedError('Unsupported platform');
+      throw UnsupportedError('Unsupported platform');
     }
   }
 
